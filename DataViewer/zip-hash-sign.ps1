f9ed355eb06ac27af1dfc527feac7c03e9edc95c
Narria <cabarius@mac.com>
2021-11-07 12:23:43 -0800

    fixed name of auto generated zip file

--- DataViewer/zip-hash-sign.ps1
+++ DataViewer/zip-hash-sign.ps1
@@ -1,7 +1,7 @@
 ﻿#New-Item -ItemType Directory -Force -Path Final
 
 $appPath = (get-item $pwd).parent.parent.FullName
-$fileName = "0ToyBox0.zip"
+$fileName = "DataViewer.zip"
 $fullName = $pwd.Path + "\..\" + $fileName
 Compress-Archive -Path '*' -DestinationPath $fullName -Force
 #Compress-Archive -Path '*.dll', '*.json', '*.pdb' -DestinationPath $fullName -Force
